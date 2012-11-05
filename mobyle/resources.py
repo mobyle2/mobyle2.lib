a9b1d45fcde3a4c190aea8c9b1efd5b7eeb920a6
Emeline <emeline.legros@ibcp.fr>
2012-11-05 17:56:41 +0100

    Remove extra copied files

--- mobyle/resources.py
+++
@@ -1,8 +0,0 @@
-from pyramid.security import Allow
-from pyramid.security import Everyone
-from pyramid.security import Authenticated
-
-class Root(object):
-    def __init__(self, request):
-        self.__acl__ = [ (Allow, Authenticated, 'viewauth') ]
-        self.request = request
