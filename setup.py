ea468d5044f96e91fcfc0f1071baf6106b562d9c
Bertrand Néron <bneron@pasteur.fr>
2012-12-11 18:24:24 +0100

    set mobyle.lib as a subproject of mobyle in site-packages
    all python libraries must be install in site-packages under it's right
    subpackages
    the top level package is mobyle
    the libraries of mobyle2.lib are in mobyle.common package
    
    add a setup.py and rename some directories to install libraries in right place

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from distutils.command.install_egg_info import install_egg_info
+from distutils.command.build import build
+from distutils.versionpredicate import VersionPredicate
+import time
+import sys
+
+class check_and_build( build ):
+    def run(self):
+        chk = True
+        for req in require_pyt:
+            chk &= self.chkpython(req)
+        for req in require_mod:
+            chk &= self.chkmodule(req)
+        if not chk:
+            sys.exit(1)
+        build.run( self )
+
+    def chkpython(self, req):
+        chk = VersionPredicate(req)
+        ver = '.'.join([str(v) for v in sys.version_info[:2]])
+        if not chk.satisfied_by(ver):
+            print >> sys.stderr, "Invalid python version, expected %s" % req
+            return False
+        return True
+
+    def chkmodule(self, req):
+        chk = VersionPredicate(req)
+        try:
+            mod = __import__(chk.name)
+        except:
+            print >> sys.stderr, "Missing mandatory %s python module" % chk.name
+            return False
+        for v in [ '__version__', 'version' ]:
+            ver = getattr(mod, v, None)
+            break
+        try:
+            if ver and not chk.satisfied_by(ver):
+                print >> sys.stderr, "Invalid module version, expected %s" % req
+                return False
+        except:
+            pass
+        return True
+
+
+require_pyt = [ 'python (>=2.7, <3.0)' ]
+require_mod = []
+
+setup(name        = 'mobyle.lib',
+      version     =  time.strftime("%Y-%m-%d"),
+      author      = "Émeline Legros, Hervé Ménager, Olivier Sallou, Adrien Saladin",
+      author_email = "emeline.legros@ibcp.fr",
+      license      = "GPLv3" ,
+      url = "https://github.com/mobyle2/mobyle2.lib",
+      download_url = "https://github.com/mobyle2/mobyle2.lib",
+      description  = "common libraries to Mobyle2 project",
+      classifiers = [
+                     'License :: GPLv3' ,
+                     'Operating System :: POSIX' ,
+                     'Programming Language :: Python' ,
+                     'Topic :: Bioinformatics' ,
+                    ] ,
+      packages    = ['mobyle' , 'mobyle.common'],
+      package_dir = {'': '.'},
+      cmdclass= { 'build' : check_and_build }
+      )
