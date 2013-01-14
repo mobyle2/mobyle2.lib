f8020fd01ec21b3a239bb57c8308580724ae2ffb
Olivier Sallou <olivier.sallou@irisa.fr>
2013-01-14 14:47:08 +0100

    add Program for mobyle2.web

---
+++ mobyle/common/program.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import abc
+from abc import ABCMeta
+from abc import abstractmethod
+from mobyleError import MobyleError
+
+from mongokit import Document
+
+from mobyle.common import session
+from mobyle.common.config import Config
+
+class Program(Document):
+    """
+    Empty class for the moment, only used as reference
+    """
+
+    __collection__ = 'programs'
+    __database__ = Config.config().get('app:main','db_name')
+
+    structure = { 'name' : basestring, 'public' : bool }
+
+    default_values = { 'public': True }
+
+    required_fields = [ 'name' ]
+
+
+session.register([Job])
+
