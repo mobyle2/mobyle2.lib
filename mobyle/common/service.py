4fe8b7a3c4674244ec693f23c2f69199c195b940
Hervé Ménager <hmenager@pasteur.fr>
2013-01-22 17:30:25 +0100

    class hierarchy skeleton for services.
    added a class hierarchy for services (software, package, program,
    etc.) and separated the packages and services into two different
    collections.

--- mobyle/common/service.py
+++ mobyle/common/service.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Jan 22, 2013
 
+@author: Hervé Ménager
+@contact: hmenager@pasteur.fr
+@organization: Institut Pasteur
+@license: GPLv3
+"""
 import abc
 from abc import ABCMeta
 from abc import abstractmethod
-
+from bson.dbref import DBRef
 from mongokit import Document
 
 from mobyle.common import session
@@ -13,12 +20,12 @@ import mf.annotation
 from mf.annotation import *
 
 @mf_decorator
-class Service(Document):
+class Software(Document):
     """
-    
+    top-level abstract element for different services and packages
+    describes the common properties of these levels.     
     """
 
-    __collection__ = 'service'
     __database__ = Config.config().get('app:main','db_name')
 
     structure = { 'name' : basestring,
@@ -61,4 +68,25 @@ class Service(Document):
 
     required_fields = [ 'name' ]
 
+class Package(Software):
+    """
+    a package is a group of services.
+    """
+    __collection__ = 'packages'
+    pass
 
+class Service(Software):
+    """
+    a service is an executable piece of software
+    """
+    __collection__ = 'services'
+    structure = {
+                  # package reference
+                  'package' : Package
+                }
+
+class Program(Service):
+    """
+    a program is a command line tool
+    """
+    structure = {}
