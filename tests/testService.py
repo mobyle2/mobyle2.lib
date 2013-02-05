62fc8d5aa7bc15f423944b134724563558109726
Hervé Ménager <hmenager@pasteur.fr>
2013-02-05 14:05:22 +0100

    service description tests
    added tests for paragraphs and parameters class hierarchy
    in services.

--- tests/testService.py
+++ tests/testService.py
@@ -4,10 +4,9 @@ from mobyle.common.service import *
 
 import pymongo
 
+from mongokit import ValidationError
 from mobyle.common import users
-
 import mobyle.common
-
 from mobyle.common  import session
 
 import mobyle.common.connection
@@ -26,6 +25,9 @@ class TestUser(unittest.TestCase):
          object.delete()
 
     def test_insert(self):
+        """
+        test basic creation of an almost empty service
+        """
         service = mobyle.common.session.Service()
         service['name'] = "test_service"
         service.save()
@@ -34,3 +36,39 @@ class TestUser(unittest.TestCase):
         for service in services_list:
           count+=1
         self.assertTrue(count==1)
+
+    def test_inputs_creation(self):
+        """
+        test creation of a service with an input
+        """
+        service = mobyle.common.session.Service()
+        service['name'] = "test_service_with_inputs"
+        inputs = InputParagraph()
+	service['inputs']=inputs
+	input_1 = InputParameter()
+        input_1['name']='test_input'
+        inputs['children'].append(input_1)
+        inputs.validate()
+        service.validate()
+        service.save()
+
+    def test_invalid_inputs_creation(self):
+        """
+        test creation of a service with an output
+        in the inputs list, which should raise
+        an error
+        """
+        #TODO: service creation code below is commented for now,
+        # as the validation for inputs paragraph
+        # is not automatically triggered when
+        # validating or saving the Service object
+        #service = mobyle.common.session.Service()
+        #service['name'] = "test_service_with_inputs"
+        inputs = InputParagraph()
+        #service['inputs']=inputs
+        output = OutputParameter()
+        output['name']='test_output'     
+        inputs['children'].append(output)
+        self.assertRaises(ValidationError,inputs.validate)
+        #service.validate()
+        #service.save()
