19495f0c29f703efd0924cc9f14eec43c4c0521e
Hervé Ménager <herve.menager@gmail.com>
2013-02-10 22:40:59 +0100

    code gardening

--- mobyle/common/service_migration.py
+++ mobyle/common/service_migration.py
@@ -10,15 +10,15 @@ Created on Feb 08, 2013
 import sys
 import xml.etree.cElementTree as ET
 from xml2json import elem_to_internal
-from service import Program
+#from service import Program
 
 #import mobyle.common
-from mobyle.common import session
+#from mobyle.common import session
 
 import mobyle.common.connection
 mobyle.common.connection.init_mongo("mongodb://localhost/")
 
-def parse_software(d,s):
+def parse_software(d, s):
     """
     parse top-level elements of the software
     """
@@ -39,13 +39,13 @@ def program_parse(s_dict):
     p = parse_software(s_dict['head'], p)
     p.save()
 
-filemasks = sys.argv[1:]
-for filename in filemasks: 
-    print 'processing %s...' % filename
-    # parse the XML into memory
-    elem = ET.fromstring(open(filename).read())
-    # create the JSON object
-    service = elem_to_internal(elem)
-    #print service
-    if service.has_key('program'):
-        program_parse(service['program'])
+if __name__ == '__main__':
+    filenames = sys.argv[1:]
+    for filename in filenames: 
+        print 'processing %s...' % filename
+        # parse the XML into memory
+        elem = ET.fromstring(open(filename).read())
+        # create the JSON object
+        service = elem_to_internal(elem)
+        if service.has_key('program'):
+            program_parse(service['program'])
