882294d12a0bec245981cad6c3978250d62ea8ad
Hervé Ménager <herve.menager@gmail.com>
2013-02-10 22:20:26 +0100

    work in progress on service migration

--- mobyle/common/service_migration.py
+++ mobyle/common/service_migration.py
@@ -12,8 +12,40 @@ import xml.etree.cElementTree as ET
 from xml2json import elem_to_internal
 from service import Program
 
+#import mobyle.common
+from mobyle.common import session
+
+import mobyle.common.connection
+mobyle.common.connection.init_mongo("mongodb://localhost/")
+
+def parse_software(d,s):
+    """
+    parse top-level elements of the software
+    """
+    s['name'] = d['name']
+    s['version'] = d.get('version', 'unspecified')
+    s['title'] = d['doc']['title']
+    s['description'] = d['doc']['description']['text']['#text']
+    s['authors'] = d['doc'].get('authors', 'unspecified')
+    
+    return s
+
+def program_parse(s_dict):
+    """
+    create a program object from a dictionary
+    "Mobyle1-style"
+    """
+    p = mobyle.common.session.Program()
+    p = parse_software(s_dict['head'], p)
+    p.save()
+
 filemasks = sys.argv[1:]
 for filename in filemasks: 
     print 'processing %s...' % filename
+    # parse the XML into memory
     elem = ET.fromstring(open(filename).read())
-    print elem_to_internal(elem)
+    # create the JSON object
+    service = elem_to_internal(elem)
+    #print service
+    if service.has_key('program'):
+        program_parse(service['program'])
