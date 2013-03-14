fd701fdcbbef79c4a30374843504cfb45e900eee
Bertrand Néron <bneron@pasteur.fr>
2013-03-14 14:58:16 +0100

    add new states in status

--- docs/source/job.rst
+++ docs/source/job.rst
@@ -13,17 +13,17 @@ Job API reference
    :private-members:
    :special-members:
 
-WorkflowJob API reference
-=========================
- .. automodule:: mobyle.common.job
-   :members: WorkflowJob  
-   :private-members:
-   :special-members:
-
 ClJob API reference
 ===================
  .. automodule:: mobyle.common.job
    :members: ClJob  
    :private-members:
    :special-members:
-
+   
+   
+WorkflowJob API reference
+=========================
+ .. automodule:: mobyle.common.job
+   :members: WorkflowJob  
+   :private-members:
+   :special-members:
--- mobyle/common/job.py
+++ mobyle/common/job.py
@@ -15,8 +15,8 @@ from mf.annotation import mf_decorator
 import logging
 _log = logging.getLogger(__name__)
 
-from mobyleError import MobyleError
 from .config import Config
+from .connection import connection
 
 
 class Status(SchemaDocument):
@@ -24,9 +24,13 @@ class Status(SchemaDocument):
 
     """the system is not able to determine the status of the job"""
     UNKNOWN = u'unknown'
-    """the environment of the job is building (working directory creation, building command line, ...)"""
+    """init the job creation (working directory creation, job settings,...) the status changed for BUILDING when the user click on "run" """
+    INIT = u'init'
+    """the environment of the job is building ( building command line, ...)"""
     BUILDING = u'building'
-    """the job has been submitted to the execution system to be running"""
+    """The job is ready to be handle by the execution engine"""
+    TO_BE_SUBMITTED = u'to be submitted'
+    """the job has been submitted to the execution system"""
     SUBMITTED = u'submitted'
     """the job is pending in the execution system note that some system cannot pend a job (SYS)""" 
     PENDING = u'pending'
@@ -40,10 +44,14 @@ class Status(SchemaDocument):
     KILLED = u'killed'
     """the job is hold by the execution system"""
     HOLD = u'hold'
-    
+    """the job is suspended by the user. when the user resume a paused job the status become TO_BE_SUBMITTED"""
+    PAUSE = u'pause'
+        
     structure = {
                   'code' : IS(UNKNOWN,
+                              INIT,
                               BUILDING,
+                              TO_BE_SUBMITTED,
                               SUBMITTED,
                               PENDING,
                               RUNNING,
@@ -51,86 +59,68 @@ class Status(SchemaDocument):
                               ERROR,
                               KILLED,
                               HOLD,
-                              ),
-                  'message' : basestring
+                              PAUSE
+                              )
                   }
-    
 
-    
-    def __init__(self , code , message = '',
-                 doc = None, gen_skel = True, gen_auth_types = True,
-                 validate = True, lang = 'en', fallback_lang = 'en'):
-        """
-        :param code: the code of the status 
-        :type code: integer 
-        :param string: the code of the status representing by a string
-        :type string: string
-        :param message: the message associated to the status
-        :type message: string
-        """
-        super(Status , self).__init__( doc = doc, gen_skel = gen_skel, gen_auth_types = gen_auth_types,
-                                       validate = validate, lang = lang, fallback_lang = fallback_lang)
-        self.code = code
-
-        if message:
-            try:
-                str( message )
-            except Exception , err:
-                #s_log.error( "Status received an non valid message: %s : %s"%( message, err ) , exc_info = True )
-                raise MobyleError , err
-            self.message = message
-        else:
-            self.message = ''
     
     def __eq__(self , other):
         """
         two Status instances are equals if there code and message are equals
-       
+        
+        :return: True if this object is equal to the other, False otherwise
+        :rtype: boolean
         """
-        return self.code == other.code and self.message == other.message
+        return self.code == other.code
+
     
     def __ne__(self , other ):
         """
-        two Status instances are not equals if they are different code or message
-       
+        two Status instances are not equals if they are different code
+        
+        :return: True if this object is not equal to the other, False otherwise
+        :rtype: boolean
         """
-        return self.code != other.code or self.message != other.message
+        return self.code != other.code
+
     
     def __str__(self):
         """
-        string representation of a Status instance
-       
+        :return: The string representation of a Status instance.
+        :rtype: string
         """
         return self.code 
         
     
     def is_ended(self):
         """
-        :returns: True if the status is among the following ones :
-                  * "finished", the job is finished without error from Mobyle
-                  * "error", the job has failed due to a MobyleError 
-                  * "killed", the job has been removed by the user, or killed by the admin
+        :return: True if the status is among the following ones :
         
-        :rtype: bool
+                 * "finished", the job is finished without error from Mobyle
+                 * "error", the job has failed due to a MobyleError 
+                 * "killed", the job has been removed by the user, or killed by the admin
         
+        :rtype: boolean
         """
         return self.code in ( self.FINISHED, self.ERROR, self.KILLED )
 
+
     def is_on_error(self):
         """
+        :return: True if the status is among the following ones :
         
-        :returns: True if the status is among the following ones :
-                  * "error", the job has failed due to a MobyleError 
-                  * "killed", the job has been removed by the user, or killed by the admin
+                 * "error", the job has failed due to a MobyleError 
+                 * "killed", the job has been removed by the user, or killed by the admin
         
-        :rtype: bool
-        
+        :rtype: boolean
         """
         return self.code in ( self.ERROR, self.KILLED )
+
         
     def is_queryable(self):
         """
-        :returns: True if the status is among the following ones :
+        :return: True if the status is among the following ones :
+        
                  * "submitted", the job.run method has been called
                  * "pending", the job has been submitted to the batch manager but wait for a slot 
                  * "running", the job is running in the batch manager
@@ -140,27 +130,30 @@ class Status(SchemaDocument):
         
         """
         return self.code in( self.SUBMITTED, self.PENDING, self.RUNNING, self.HOLD )
+
     
     def is_known(self):
-        """if the system know the status of the job
-        
+        """
+        :return: True if the system know the status of the job, False otherwise
         :rtype: bool
-        
         """
         return self.code != self.UNKNOWN
+
     
     def is_submittable(self):
-        """:returns: True if the job is ready to be submitted to a batch system :
-        
+        """
+        :return: True if the job is ready to be submitted to a batch system.
         :rtype: bool
-        
         """
         return self.code == self.BUILDING
 
-class AbstractJob(Document):
+
+class Job(Document):
     """
-    AbstractJobRef is an abstract class that describes the common interface of JobRef and WorkflowRef
+    Job is an abstract class that describes the common interface of all jobs
     """     
+    use_dot_notation = True
+    
     __collection__ = 'jobs'
     __database__ = Config.config().get('app:main','db_name')
 
@@ -168,90 +161,57 @@ class AbstractJob(Document):
                  'name' : basestring,
                  'status' : Status,
                  'owner' : basestring,
+                 'message' : basestring,
+                 'end_time' : datetime.datetime,
                   }
-    
-    def __init__(self, status, owner, **kwargs):
-        """
-        :param id: the identifier of this jobRef
-        :type id: string
-        :param create_time: the time of job creation
-        :type create_time: time.struct_time
-        :param status: the status of this jobRef
-        :type status: `:class:` object
-        :param owner: owner id of the job: either a workflow (local|remote) or a userspace (local|remote)
-        :type owner: string
-        
-        """
-        super(AbstractJob , self).__init__(**kwargs)
-        self._owner = owner
-        self.status = status
+
         
     def __cmp__(self, other):
         """
-        :param other: a JobRef I want to comared with self
+        :param other: a :class:`mobyle.common.job.Job` object I want to comared with self
         :type other: an AbstractJobRef instance
-        :returns: negative int if the other object was created before this one, positive integer if other is newer, or 0 if they are created at the same time.
+        :return: negative int if the other object was created before this one, 
+                 positive integer if other is newer, or 0 if they are created at the same time.
         :rtype: Integer 
-        
         """
         return cmp( self.create_time , other.create_time )
-    
 
-    @property
-    def owner(self):
-        """
-        :return: the owner of a job. It can be a user space or a workflow
-        :rtype: ???
-         
-        """
-        return self.owner
     
     @property
     def create_time(self):
         """
-        :returns: the time of job creation
+        :return: the time of job creation
         :rtype: datetime.datetime object
-        
         """
-        return self["_id"].generation_time
+        return self._id.generation_time
     
 
     def must_be_notified(self):
         """
-        :returns: True if a notification must be send a the end of job. False otherwise 
-        
+        :return: True if a notification must be send a the end of job. False otherwise 
+        :rtype: boolean
         """
         return NotImplementedError
         
         
-@mf_decorator    
-class Job(AbstractJob):
+@mf_decorator   
+@connection.register
+class ClJob(Job):
     """
-    AbstractJob implementation for a simple job
+    Job implementation for a Command line  job
     """ 
+    
+    use_dot_notation = True
+    
     structure = { 
-                'end_time' : datetime.datetime,
                 'has_been_notified' : bool
                 }
     
-    def __init__(self, name, status, owner, **kwargs ):
-        """
-        :param name: the user name of this job
-        :type name: string
-        :param status: the status of this jobRef
-        :type status: L{status} instance
-        :param owner: "owner" id of the job: either a workflow( local or remote) or a userspace(local or remote)
-        :type owner: ???
-        
-        """
-        super(JobRef , self).__init__(name, status, owner, **kwargs )
-        self.end_time = None
-        self.has_been_notified = False
-
+    
     def must_be_notified(self):
         """
-        :returns: True if a notification must be send a the end of job. False otherwise 
-        
+        :return: True if a notification must be send a the end of job. False otherwise 
+        :rtype: bool
         """
         delay = 60
         if self.end_time:
@@ -260,3 +220,10 @@ class Job(AbstractJob):
             return False
 
     
+@mf_decorator   
+@connection.register
+class WorkflowJob(Job):
+    """
+    not implemented for now
+    """
+    pass
