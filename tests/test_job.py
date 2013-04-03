0068cad8c6f89c988bcf56d76ad240d5dffe5d71
Bertrand Néron <bneron@pasteur.fr>
2013-04-03 09:57:26 +0200

    fix test_insert

--- tests/test_job.py
+++ tests/test_job.py
@@ -2,6 +2,7 @@
 
 import unittest
 import os.path
+import time
 #a config object must be instantiated first for each entry point of the application
 from mobyle.common.config import Config
 config = Config( os.path.join( os.path.dirname(__file__), 'test.conf'))
@@ -33,11 +34,24 @@ class TestJob(unittest.TestCase):
         
         job_list = connection.ClJob.find({'name': 'first job'})
         count = 0
-        for user in user_list:
+        for user in job_list:
             count+=1
         self.assertEqual(count, 1)
     
-
+    def test_cmp(self):
+        status = Status(Status.INIT)
+        job_1 = connection.ClJob()
+        job_1.name = "first job"
+        job_1.status = status
+        job_1.owner = "me"
+        job_1.save()
+        time.sleep(1)
+        job_2 = connection.ClJob()
+        job_2.name = "first job"
+        job_2.status = status
+        job_2.owner = "me"
+        job_2.save()
+        self.assertGreater(job_2 , job_1)
     
 if __name__ == '__main__':
     unittest.main()
