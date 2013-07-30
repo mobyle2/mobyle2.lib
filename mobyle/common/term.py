fcc432dc032b4304efede4d7e94ab90a6d78b2a8
Hervé Ménager <hmenager@pasteur.fr>
2013-07-30 11:29:31 +0200

    refactor Type code to reuse for other kinds of ontology terms

---
+++ mobyle/common/term.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mar 7, 2013
+
+@author: Olivia Doppelt-Azeroual
+@author: Hervé Ménager
+@contact: odoppelt@pasteur.fr
+@contact: hmenager@pasteur.fr
+@organization: Institut Pasteur, CIB
+@license: GPLv3
+"""
+
+from mongokit import Document
+
+from mf.annotation import mf_decorator
+
+from mobyle.common.connection import connection
+from mobyle.common.config import Config
+
+from mf.views import MF_READ, MF_EDIT
+
+class AbstractTerm(Document):
+    """
+    Abstract class for term storage
+    should not be used to define Term classes,
+    please inherit from Term class instead
+    """
+
+    __database__ = Config.config().get('app:main','db_name')
+
+    structure = {
+        'id': basestring,
+        'name': basestring,
+        'definition': basestring,
+        'synonyms': [basestring],
+        'is_obsolete': bool
+        }
+
+    def my(self, control, request, authenticated_userid=None):
+        user = connection.User.find_one({'email' : authenticated_userid})
+        admin_mode = 'adminmode' in request.session
+        if control == MF_READ or (user and user['admin'] and admin_mode):
+            return {}
+        else:
+            return None
+
+@mf_decorator
+@connection.register
+class Term(AbstractTerm):
+    """
+    Term information storage, including subclass references
+    """
+
+
+    structure = {
+        'subclassOf': [AbstractTerm]
+        }
+
--- mobyle/common/type.py
+++ mobyle/common/type.py
@@ -14,69 +14,29 @@ from mf.annotation import mf_decorator
 
 from mobyle.common.connection import connection
 from mobyle.common.config import Config
+from mobyle.common.term import Term
 
 from mf.views import MF_READ, MF_EDIT
 
-class AbstractType(Document):
-    """
-    Abstract class for type information based on EDAM ontology
-    """
-
-    __collection__ = 'types'
-    __database__ = Config.config().get('app:main','db_name')
-
-    structure = {
-        'id': basestring,
-        'name': basestring,
-        'definition': basestring,
-        'synonyms': [basestring],
-        'is_obsolete': bool
-        }
-
-    def my(self, control, request, authenticated_userid=None):
-        user = connection.User.find_one({'email' : authenticated_userid})
-        admin_mode = 'adminmode' in request.session
-        if control == MF_READ or (user and user['admin'] and admin_mode):
-            return {}
-        else:
-            return None
-
 @mf_decorator
 @connection.register
-class Type(AbstractType):
+class Type(Term):
     """
     type information for data based on EDAM ontology
     """
-
-
-    structure = {
-        'subclassOf': [AbstractType]
-        }
-
-class AbstractFormat(Document):
-    """
-    Abstract class for format information based on EDAM ontology
-    """
-    structure = {
-        'id': basestring,
-        'name': basestring,
-        'definition': basestring,
-        'comment': basestring, 
-        'synonyms': [basestring], 
-        'isFormatOf': [Type]
-        }
+    __collection__ = 'types'
 
 @mf_decorator
 @connection.register
-class Format(AbstractFormat):
+class Format(Term):
     """
     format information based on EDAM ontology
     """
     __collection__ = 'formats'
-    __database__ = Config.config().get('app:main','db_name')
 
     structure = {
-        'subclassOf': [AbstractFormat]
+        'isFormatOf': [Type],
+        'comment': [basestring]
         }
 
     
