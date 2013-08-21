4516566ad718b705514f4d7b618002f0568f72b2
Bertrand Néron <bneron@pasteur.fr>
2013-08-21 10:18:14 +0200

    remove compatiblity code for old python version as we support python 2.7 only

--- mobyle/common/mobyleError.py
+++ mobyle/common/mobyleError.py
@@ -1,46 +1,19 @@
 # -*- coding: utf-8 -*-
-'''
-Created on Nov. 12, 2012
 
-@author: E. LEGROS
-@contact: emeline.legros@ibcp.fr
-@license: GPLv3
-'''
-
+#===============================================================================
+# Created on Aug 21, 2013
+# 
+# @author: Bertrand Néron
+# @contact: bneron <at> pasteur <dot> fr
+# @organization: Institut Pasteur
+# @license: GPLv3
+#===============================================================================
 
 
 from exceptions import Exception
 
 class MobyleError(Exception):
     """
-    MobyleError is a class that manages exceptions. 
+    MobyleError is a class that manages Mobyle specific exceptions. 
     """
-    
-    def __init__(self, msg = None):
-	"""
-	:param msg: exception information message
-	:type msg: string
-	"""
-	
-        self._message = str( msg )
-    
-    def _get_message(self):
-	"""
-	:return: exception information message
-	:rtype: string
-	"""
-	
-        return self._message
-    #workaround to ensure Mobyle compatibility
-    #with either python 2.5 and python 2.6
-    #as self.message is deprecated in python 2.6
-    message = property( fget = _get_message )
-    
-    def __str__(self, *args, **kwargs):
-	"""
-	:return: exception information message
-	:rtype: string
-	"""
-	
-        return self.message
-
+    pass
