0462140991257e767fd8511adc550a974aac92dd
Hervé Ménager <hmenager@pasteur.fr>
2014-01-27 16:30:06 +0100

    add/correct doc on ProjectData.

--- mobyle/common/project.py
+++ mobyle/common/project.py
@@ -172,9 +172,15 @@ class ProjectData(ProjectDocument):
                  'tags': [basestring],
                  'project': ObjectId,
                  'data': AbstractData,
+                 # see ObjectManager. Only READY data can be used
                  'status': int,
+                 # set to True to avoid automatic deletion
+                 # by cleanup tasks
                  'persistent': bool,
+                 # path to the container directory for the data
+                 # if it is not stored in the datamanager pairtree
                  'path': basestring,
+                 # set to True to override project visibility
                  'public': bool
                  #TODO: add data provenance information
                 }
@@ -195,7 +201,7 @@ class ProjectData(ProjectDocument):
         return self['data']
 
     def status(self, status=None):
-        '''Update schema if parameter is not None, return schema'''
+        '''Update status if parameter is not None, return status'''
         if status is not None:
             self['status'] = status
             self.save()
