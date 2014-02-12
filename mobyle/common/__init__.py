d9cefd0e0775be1272217c609cb7543f5357bd8c
Hervé Ménager <hmenager@pasteur.fr>
2014-02-12 18:12:59 +0100

    add all modules to mobyle.common __all__.

--- mobyle/common/__init__.py
+++ mobyle/common/__init__.py
@@ -8,3 +8,7 @@ Created on Nov. 12, 2012
 '''
 __import__('pkg_resources').declare_namespace('mobyle')
 
+__all__ = ["classification", "config", "connection", "data",
+           "detector", "job", "mobyleConfig", "mobyleError",
+           "objectmanager", "project", "service", "term", "tokens",
+           "type", "users"]
